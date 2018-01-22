b58b0854b9a0815ccd92df8db3cdc3903e89da72
Jeanderson Candido <jeandersonbc@gmail.com>
2018-01-22 11:54:17 -0300

    Added Scope demo

---
+++ src/main/java/com/example/springdemo/demo1/LifecycleAndScopeDemo.java
@@ -0,0 +1,23 @@
+package com.example.springdemo.demo1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.example.springdemo.demo1.beans.Foo;
+
+public class LifecycleAndScopeDemo {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("lifecycle-scope-context.xml");
+
+		Foo foo1 = context.getBean("fooSingleton", Foo.class);
+		Foo foo2 = context.getBean("fooSingleton", Foo.class);
+		System.out.format("Expected true (singleton scope): %s%n", foo1 == foo2);
+
+		Foo foo3 = context.getBean("fooPrototype", Foo.class);
+		Foo foo4 = context.getBean("fooPrototype", Foo.class);
+		System.out.format("Expected false (prototype scope): %s%n", foo3 == foo4);
+
+		context.close();
+	}
+
+}
---
+++ src/main/java/com/example/springdemo/demo1/beans/Foo.java
@@ -0,0 +1,8 @@
+package com.example.springdemo.demo1.beans;
+
+/**
+ * Dummy class to demonstrate life-cycle and scope
+ */
+public class Foo {
+
+}
