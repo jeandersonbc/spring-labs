79a71882b32b3eb614987f2b47575c19c9a7f425
Jeanderson Candido <jeandersonbc@gmail.com>
2018-01-22 10:43:29 -0300

    Cleanup main

--- src/main/java/com/example/springdemo/demo1/HelloSpringApp.java
+++ src/main/java/com/example/springdemo/demo1/HelloSpringApp.java
@@ -10,18 +10,15 @@ public class HelloSpringApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		// retrieve bean from spring container
-//		Coach theCoach = context.getBean("myCoach", Coach.class);
-		Coach theOtherCoach = context.getBean("myOtherCoach", Coach.class);
+		Coach theCoach = context.getBean("myOtherCoach", Coach.class);
 
 		// call methods on the bean
-//		System.out.println(theCoach.getDailyWorkout());
-		System.out.println(theOtherCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyWorkout());
 
 		// notice we didn't have to bother on creating and instantiating the
 		// HappyFortuneService class. this is dependency injection
-//		System.out.println(theCoach.getDailyFortune());
-		System.out.println(theOtherCoach.getDailyFortune());
-		System.out.println(theOtherCoach);
+		System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach);
 
 		// close app context
 		context.close();
