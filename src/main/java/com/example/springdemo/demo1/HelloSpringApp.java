4eaa6f757712cdd025f34c1aa30407a685210cbb
Jeanderson Candido <jeandersonbc@gmail.com>
2018-01-22 10:24:55 -0300

    Demonstrating literal values injection

--- src/main/java/com/example/springdemo/demo1/HelloSpringApp.java
+++ src/main/java/com/example/springdemo/demo1/HelloSpringApp.java
@@ -10,17 +10,18 @@ public class HelloSpringApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		// retrieve bean from spring container
-		Coach theCoach = context.getBean("myCoach", Coach.class);
+//		Coach theCoach = context.getBean("myCoach", Coach.class);
 		Coach theOtherCoach = context.getBean("myOtherCoach", Coach.class);
 
 		// call methods on the bean
-		System.out.println(theCoach.getDailyWorkout());
+//		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theOtherCoach.getDailyWorkout());
 
 		// notice we didn't have to bother on creating and instantiating the
 		// HappyFortuneService class. this is dependency injection
-		System.out.println(theCoach.getDailyFortune());
+//		System.out.println(theCoach.getDailyFortune());
 		System.out.println(theOtherCoach.getDailyFortune());
+		System.out.println(theOtherCoach);
 
 		// close app context
 		context.close();
--- src/main/java/com/example/springdemo/demo1/beans/CricketCoach.java
+++ src/main/java/com/example/springdemo/demo1/beans/CricketCoach.java
@@ -6,6 +6,8 @@ import com.example.springdemo.demo1.FortuneService;
 public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
+	private String email;
+	private String team;
 
 	@Override
 	public String getDailyWorkout() {
@@ -19,5 +21,18 @@ public class CricketCoach implements Coach {
 
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("[Coach: %s, team: %s, email: %s]", getClass().getSimpleName(), this.team, this.email);
 	}
 }
