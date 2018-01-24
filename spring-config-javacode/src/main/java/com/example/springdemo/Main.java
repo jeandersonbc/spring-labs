fac11396a648ef63b7aecfb0388b4996eeadd713
Jeanderson Candido <jeandersonbc@gmail.com>
2018-01-24 12:46:51 -0300

    small refactoring

--- spring-config-javacode/src/main/java/com/example/springdemo/Main.java
+++ spring-config-javacode/src/main/java/com/example/springdemo/Main.java
@@ -1,11 +1,13 @@
 package com.example.springdemo;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 public class Main {
 
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		// AnnotationConfig and ClassPathXml inherit from AbstractApplicationContext
+		AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
 		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 		System.out.println(theCoach.getDailyWorkout());
