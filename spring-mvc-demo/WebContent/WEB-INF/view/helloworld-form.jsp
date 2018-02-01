78f508f4aa6ce4e145a24030fe1b3c3b355bf220
Jeanderson Candido <jeandersonbc@gmail.com>
2018-02-01 19:20:08 -0300

    Demonstrating the use of RequestParam

--- spring-mvc-demo/WebContent/WEB-INF/view/helloworld-form.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/helloworld-form.jsp
@@ -1,5 +1,3 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,7 +6,7 @@
 </head>
 <body>
 
-	<form action="processForm" method="GET">
+	<form action="processFormTwo" method="GET">
 		<input type="text" name="studentName" placeholder="What's your name?" />
 		<input type="submit" />
 	</form>
--- spring-mvc-demo/src/com/example/springdemo/HelloController.java
+++ spring-mvc-demo/src/com/example/springdemo/HelloController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -23,6 +24,17 @@ public class HelloController {
 	public String process(HttpServletRequest request, Model model) {
 		String name = request.getParameter("studentName");
 		String message = name.toUpperCase();
+		model.addAttribute("message", message);
+		return "process-form";
+	}
+
+	/*
+	 * RequestParam is a shortcut to access the HTTP Request and get the parameter
+	 * from the request. It automatically binds to the given variable.
+	 */
+	@RequestMapping("/processFormTwo")
+	public String processTwo(@RequestParam("studentName") String name, Model model) {
+		String message = String.format("Hey %s!", name.toUpperCase());
 		model.addAttribute("message", message);
 		return "process-form";
 	}
